90780949c7a763c3ff5eb5b25138482865142938
carthebottlecold <qudcks139@daum.net>
2025-02-04 23:59:22 +0900

    회원 가입 로직 기능 구현
    프론트엔드 및 백엔드 작업  signup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
 
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
@@ -63,9 +64,15 @@ async def get_image(item_id):
 
 @app.post('/signup')
 def signup(id:Annotated[str,Form()],
-           password:Annotated[str,Form()]):
-    print(id,password)
-    return'200'
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+  cur.execute(f"""
+              INSERT INTO users(id,name,email,password)
+              VALUES('{id}','{name}','{email}','{password}')
+              """)
+  con.commit()
+  return'200'
 
 app.mount("/", StaticFiles(directory="frontend",html=True), name="frontend")
 
